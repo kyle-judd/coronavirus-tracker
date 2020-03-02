64b39bec0f5568967d562f6f28bbd44f37bcbd84
kyle-judd <juddkyle1408@gmail.com>
2020-03-01 23:04:39 -0500

    added csv reader to read response body

--- src/main/java/com/kylejudd/services/CoronaVirusDataService.java
+++ src/main/java/com/kylejudd/services/CoronaVirusDataService.java
@@ -1,6 +1,7 @@
 package com.kylejudd.services;
 
 import java.io.IOException;
+import java.io.StringReader;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -8,6 +9,8 @@ import java.net.http.HttpResponse;
 
 import javax.annotation.PostConstruct;
 
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -23,6 +26,12 @@ public class CoronaVirusDataService {
 		
 		HttpResponse<String> httpResponse = client.send(request, HttpResponse.BodyHandlers.ofString());
 		
-		System.out.println(httpResponse.body());
+		StringReader csvBodyReader = new StringReader(httpResponse.body());
+		
+		Iterable<CSVRecord> records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(csvBodyReader);
+		for (CSVRecord record : records) {
+		    String state = record.get("Province/State");
+		    System.out.println(state);
+		}
 	}
 }
