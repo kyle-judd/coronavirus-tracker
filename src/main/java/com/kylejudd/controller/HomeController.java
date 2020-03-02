2beca1f08165485117f42b0fd4a709f0bd68b1d3
kyle-judd <juddkyle1408@gmail.com>
2020-03-01 23:28:33 -0500

    added virus service to controller

--- src/main/java/com/kylejudd/controller/HomeController.java
+++ src/main/java/com/kylejudd/controller/HomeController.java
@@ -1,11 +1,17 @@
 package com.kylejudd.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.kylejudd.services.CoronaVirusDataService;
+
 @Controller
 public class HomeController {
+	
+	@Autowired
+	private CoronaVirusDataService coronaVirusDataService;
 	
 	@GetMapping("/")
 	public String home(Model model) {
