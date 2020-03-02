48afcad351a7215540ee924adb7afdec8f856254
kyle-judd <juddkyle1408@gmail.com>
2020-03-01 23:22:42 -0500

    created thymeleaf template and controller

--- src/main/java/com/kylejudd/services/CoronaVirusDataService.java
+++ src/main/java/com/kylejudd/services/CoronaVirusDataService.java
@@ -6,20 +6,28 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.annotation.PostConstruct;
 
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+
+import com.kylejudd.model.LocationStats;
 
 @Service
 public class CoronaVirusDataService {
 	
 	private static String VIRUS_DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv";
+	private List<LocationStats> allStats = new ArrayList<>();
 	
 	@PostConstruct
+	@Scheduled(cron = "* * 1 * * *")
 	public void fetchVirusData() throws IOException, InterruptedException {
+		List<LocationStats> newStats = new ArrayList<>();
 		
 		HttpClient client = HttpClient.newHttpClient();
 		HttpRequest request = HttpRequest.newBuilder().uri(URI.create(VIRUS_DATA_URL)).build();
@@ -30,8 +38,14 @@ public class CoronaVirusDataService {
 		
 		Iterable<CSVRecord> records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(csvBodyReader);
 		for (CSVRecord record : records) {
-		    String state = record.get("Province/State");
-		    System.out.println(state);
+		    LocationStats locationStat = new LocationStats();
+			locationStat.setState(record.get("Province/State"));
+			locationStat.setCountry(record.get("Country/Region"));
+			locationStat.setLatestTotal(Integer.parseInt(record.get(record.size() - 1)));
+		    System.out.println(locationStat);
+		    newStats.add(locationStat);
 		}
+		
+		allStats = newStats;
 	}
 }
